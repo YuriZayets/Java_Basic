e3b18b135cd41fafd5d348b339367ee033544ffa
YuriZayets <zayetsyuri@gmail.com>
2020-11-25 23:03:30 +0300

    Reworked taskThree into double and fixed case of wrong result when one all the parameters on one axis where same.

--- Basics_of_software_code_development/Branching.java
+++ Basics_of_software_code_development/Branching.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 
 public class Branching {
     public static void main(String[] args) {
-        System.out.println(taskThree(1,0,2,0,3,0));
-        System.out.println(2.3/1.1);
     }
 
     static void taskOne(double a, double b) {
